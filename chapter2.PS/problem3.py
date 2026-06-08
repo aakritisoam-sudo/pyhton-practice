62021e555320716ff493f79edd1835479d4cb343
aakritisoam-sudo <youremail@gmail.com>
2026-06-08 15:50:27 +0530

    add practice

--- chapter2.PS/problem3.py
+++ chapter2.PS/problem3.py
@@ -1,1 +1,4 @@
-#type of variables 
+#type of variables assigned using input() function 
+
+a = input ("Enter the value of a") #input (....) will always show a string no matter what will you write inside it
+print(type(a))
---
+++ chapter2.PS/problem5.py
@@ -0,0 +1,4 @@
+# find an average of two numbers entered by the user 
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+print("The average of the two numbers is:", (a + b) / 2)
