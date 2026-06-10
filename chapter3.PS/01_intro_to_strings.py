037cf7ca678785c9782cba2684dc639e686d89c5
aakritisoam-sudo <youremail@gmail.com>
2026-06-10 13:16:27 +0530

    string started

---
+++ chapter3.PS/01_intro_to_strings.py
@@ -0,0 +1,12 @@
+name = 'aakriti' #single quoted string
+b = "aakriti" #double quoted string
+c = '''aakriti''' #triple quoted string
+
+
+nameshort = name[0:5] #start from index 0 all the way till  (5 excluding 4)
+print(nameshort)
+character1 = name[1]
+print(character1)
+
+sl = name[ind_start:ind_end] #first index included / last index is not included
+
---
+++ chapter3.PS/02_negative_slicing.py
@@ -0,0 +1,10 @@
+name = "aakriti"
+
+print(name[0:3])
+
+print(name[-4:-1]) #negative ko positive bnalo index dekhke bs 
+
+print(name[3:6])
+
+print(name[:4]) #is same as print(name[0:5])
+print(name[0:]) #is same as print(name[1:5]) {will give you full length of the string}
