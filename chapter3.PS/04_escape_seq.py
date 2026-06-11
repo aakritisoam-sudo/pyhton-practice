e900d6fead7c578a979a7456c85f72e8830a0477
aakritisoam-sudo <aakritisoam@gmail.com>
2026-06-11 13:14:43 +0530

    escape sequence

---
+++ chapter3.PS/04_escape_seq.py
@@ -0,0 +1,12 @@
+
+b = "aakriti is a good girl\nbut not a bad girl"
+print(b) #if you want to write a string in multiple lines then you have to use \n for new line
+
+c = "aakriti is a good girl\tbut not a bad girl"
+print(c) #if you want to give a tab space in the string then you have to use \t
+
+d = "aakriti is a good girl\\but not a bad girl"
+print(d) #if you want to give a backlash in the string then you have to use \\
+
+e = "aakriti is a good girl\"but not a bad girl/"
+print(e) #if you want to give a double quote in the string then you have to use \"
