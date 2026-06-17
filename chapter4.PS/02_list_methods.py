38f902e1e0b641752d267646c32a1c94a95350fd
aakritisoam-sudo <aakritisoam@gmail.com>
2026-06-17 11:47:33 +0530

    list methods

---
+++ chapter4.PS/02_list_methods.py
@@ -0,0 +1,24 @@
+friend = ["Apple", "Banana", "Cherry", 5, 345.06, False, "Aakash", "Aakriti"]
+print(friend)
+
+friend.append("akki") # this will add the value at the end of the list
+print(friend)
+
+l = [1,12,14,99,74]
+l.sort() #this will sort the list in ascending order
+print(l)
+
+a = [2,34,89,78]
+a.reverse() #this will reverse the list
+print(a)
+
+v= [1,2,3,4,5]
+v.insert(2,100) #this will insert the value at the given index
+print(v)
+
+t=[34,56,34,13,23]
+print(t.pop(3)) # this will remove the value at the given index
+print(t)
+
+s= [1,2,3,4,5]
+s.remove(3) #this will remove the value from yhe list
