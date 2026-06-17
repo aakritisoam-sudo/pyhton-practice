677b9bcdf059c16a57733408c53d8031a7fd82cf
aakritisoam-sudo <aakritisoam@gmail.com>
2026-06-17 11:32:22 +0530

    chapter 4 start

---
+++ chapter4.PS/01_list.py
@@ -0,0 +1,7 @@
+friend = ["Apple", "Banana", "Cherry", 5, 345.06, False, "Aakash", "Aakriti"]
+print(friend[0]) # Apple 
+print(friend[1]) # Banana
+friend[1] = 'Cherry' # this is valid because we are changing the value of this list not the string itself.
+print(friend) 
+ 
+print(friend[0:3]) 
