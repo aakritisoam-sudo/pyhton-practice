2c0246e12b552851a8e6a3a365704a08db9fb721
aakritisoam-sudo <aakritisoam@gmail.com>
2026-06-23 12:23:58 +0530

    tuple methods

---
+++ chapter4.PS/04_tuple_methods.py
@@ -0,0 +1,9 @@
+a = (1,234,245,678,False,True,"Aakriti")
+print(a) 
+
+no = a.count(678) 
+print(no) # this will return the count of the value of the tuple a number of times it repeated 
+
+i = a.index(678)
+print(i) # this will return the index value 
+
