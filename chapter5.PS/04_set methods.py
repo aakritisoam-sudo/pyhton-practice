095703861f681f0f9b0c6e0649422a62dcc7edd9
aakritisoam-sudo <aakritisoam@gmail.com>
2026-06-26 16:46:57 +0530

    set methods

---
+++ chapter5.PS/04_set methods.py
@@ -0,0 +1,13 @@
+s = {1,5,34,54,5,5,"Aakriti"}
+
+s.add(100) # it will add 100 to the set 
+print(s)
+
+s.remove(5) # it will remove 5 from the set
+print(s)
+
+s.pop() #it will remove a random element from the set 
+print(s)
+
+s.clear() # it will clear the set
+print (s)
