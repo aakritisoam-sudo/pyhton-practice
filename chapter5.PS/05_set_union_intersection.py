95d03bb20ae83cb3b6f58060b3f6048b65d895b4
aakritisoam-sudo <aakritisoam@gmail.com>
2026-06-26 16:52:53 +0530

    union and intersection

---
+++ chapter5.PS/05_set_union_intersection.py
@@ -0,0 +1,8 @@
+s1 = {1,23,45}
+s2 = {1,2,23,4,5}
+
+union = s1.union(s2) # it will return a new set with all elements from the both elements
+print(union)
+
+intersection = s1.intersection(s2) # it will return a new set with common elements from both sets 
+print(intersection)
