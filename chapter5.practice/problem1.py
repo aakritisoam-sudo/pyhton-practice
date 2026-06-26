515db67db4441587c19b126c99805f9f59b6c7b1
aakritisoam-sudo <aakritisoam@gmail.com>
2026-06-26 17:10:19 +0530

    chapter 5 practice

---
+++ chapter5.practice/problem1.py
@@ -0,0 +1,9 @@
+# program to create a dictionary of Hindi words with values as their english translation. Provide user with an option to look it up
+
+words = {
+    "madad": "help",
+    "pani": "water",
+    "aaram": "rest",
+}
+words = input("Enter a hindi word to look up its english translation:")
+print(words[words])
