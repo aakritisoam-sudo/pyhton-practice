79c62ab0fcc4685c0c74464160fbd013ca64db23
aakritisoam-sudo <aakritisoam@gmail.com>
2026-06-26 16:40:52 +0530

    about sets

---
+++ chapter5.PS/03_set.py
@@ -0,0 +1,8 @@
+s = {} # empty dictionary
+
+s = {1,2,3} 
+e = set() # dont use s = {} it will create an empty dictionary not a set
+s = {1,2,3,4,1,2,3}
+print(s) # in sets duplicate values are not allowed, so it will not print the same value twice 
+
+
