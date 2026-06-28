f65feff4d1ca69151c09197ea42665f77cf751d3
aakritisoam-sudo <aakritisoam@gmail.com>
2026-06-28 14:11:19 +0530

    if-if statement

---
+++ chapter6.PS/03_multiple_if_statement.py
@@ -0,0 +1,21 @@
+a = int(input("Enter your age:"))
+
+#if statement 1
+if(a%2==0):
+    print("a is even")
+
+# End of if statement 1    
+
+#if statement 2
+if(a>=18):
+    print("you are eligible to vote")
+    print("good for you")
+elif(a<0):
+    print("you are entering invalid age")    
+else:
+    print("you are not eligible to vote")
+
+# End of if statement     
+
+print("end of this program")
+
